9b28bb43687da6a4404d31ff46dee6971062b81b
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2023-04-18 12:47:51 +0200

    Update the implementation of the notification event

--- events/index.ts
+++ events/index.ts
@@ -10,46 +10,7 @@ export default (io: Server, socket: Socket) => {
   });
 
   socket.on("notification", (data) => {
-    /*
-      Data Format :
-      data: {
-        type: "friend" | "join-server" | "message",
-        to?: idUser | idChannel,
-      }
-    */
-    const notifTypes: string[] = [];
-    const notifForUser: string[] = [];
-    const notifForChannel: string[] = [];
-
-    const error = {
-      error: "invalid-data",
-      event: socket.data.eventTriggered,
-    };
-
-    // Verify if data are valid
-    if (!data.type || !notifTypes.includes(data.type)) {
-      socket.emit("error", error);
-      return;
-    }
-
-    // Send notification to a user
-    if (notifForUser.includes(data.type)) {
-      const userSocket = userSockets[data.to];
-
-      // Verify if data are valid
-      if (!data.to) {
-        socket.emit("error", error);
-        return;
-      }
-
-      io.to(userSocket.socket).emit("notification", { type: data.type });
-    } else if (notifForChannel.includes(data.type)) {
-      const userSocket = userSockets[socket.data.userId];
-
-      if (userSocket.room) {
-        io.to(userSocket.room).emit("notification", { type: data.type });
-      }
-    }
+    sendNotification(io, socket, data);
   });
 };
 
@@ -58,4 +19,51 @@ function leaveAllRooms(socket: Socket) {
     if (idRoom === socket.id) return;
     socket.leave(idRoom);
   });
+}
+
+function sendNotification(
+  io: Server,
+  socket: Socket,
+  data: Record<string, string>
+) {
+  /*
+      Data Format :
+      data: {
+        type: "friend" | "join-server" | "message",
+        to?: idUser | idChannel,
+      }
+    */
+  const notifTypes: string[] = [];
+  const notifForUser: string[] = [];
+  const notifForChannel: string[] = [];
+
+  const error = {
+    error: "invalid-data",
+    event: socket.data.eventTriggered,
+  };
+
+  // Verify if data are valid
+  if (!data.type || !notifTypes.includes(data.type)) {
+    socket.emit("error", error);
+    return;
+  }
+
+  // Send notification to a user
+  if (notifForUser.includes(data.type)) {
+    const userSocket = userSockets[data.to];
+
+    // Verify if data are valid
+    if (!data.to) {
+      socket.emit("error", error);
+      return;
+    }
+
+    io.to(userSocket.socket).emit("notification", { type: data.type });
+  } else if (notifForChannel.includes(data.type)) {
+    const userSocket = userSockets[socket.data.userId];
+
+    if (userSocket.room) {
+      io.to(userSocket.room).emit("notification", { type: data.type });
+    }
+  }
 }
